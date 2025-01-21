c80e1ad6b5adf3da35a3e7de169809e129f9d7ec
Mark <markusrr10@gmail.com>
2025-01-21 21:20:36 +0700

    doc: add api documentation

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -3,5 +3,5 @@ from controllers.auth_controllers import register_controller, login_controller
 
 auth_route = Blueprint('auth_route', __name__)
 
-auth_route.post('/api/register')(register_controller)
+auth_route.post('/api/users/register')(register_controller)
 auth_route.post('/api/auth/login')(login_controller)
