1ecdb84f93150a722520a7e552a2c93bd85d8e53
Mark <markusrr10@gmail.com>
2025-01-21 21:20:36 +0700

    doc: add api documentation

--- app.py
+++ app.py
@@ -6,11 +6,24 @@ from sqlalchemy.exc import SQLAlchemyError
 from routes.auth_route import auth_route
 from routes.user_route import user_route
 from routes.item_route import item_route
+from flask_swagger_ui import get_swaggerui_blueprint
 
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 CORS(app)
+
+swager_url = '/api/docs'
+api_url = '/static/api_docs.yml'
+swager_ui_blueprint = get_swaggerui_blueprint(
+    swager_url,
+    api_url,
+    config={
+        'app_name': 'Auction API Documentation'
+    }
+)
+
+app.register_blueprint(swager_ui_blueprint, url_prefix=swager_url)
 
 app.register_blueprint(auth_route)
 app.register_blueprint(user_route)
